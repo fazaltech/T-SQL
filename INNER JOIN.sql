b8f777f29641d4fc2ceeba37ad1e9707387ea9c6
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2021-12-24 10:51:33 +0500

    Update INNER JOIN.sql

--- INNER JOIN.sql
+++ INNER JOIN.sql
@@ -5,3 +5,21 @@ ON P.ProductSubcategoryID=PS.ProductSubcategoryID
 
 SELECT *
 FROM Production.ProductSubcategory
+
+
+
+
+SELECT PS.NAME AS ProductSubCategoryName,
+		PC.NAME AS ProductCategoryName
+FROM Production.ProductSubcategory PS
+INNER JOIN Production.ProductSubcategory PC
+ON PS.ProductCategoryID=PC.ProductCategoryID
+
+SELECT * FROM Person.Person
+
+SELECT P.FirstName,P.LastName,E.EmailAddress,PP.PhoneNumber
+FROM Person.Person P
+INNER JOIN Person.EmailAddress E
+ON P.BusinessEntityID=E.BusinessEntityID
+INNER JOIN Person.PersonPhone PP
+ON P.BusinessEntityID=PP.BusinessEntityID
