b884274363e3fccf842d36f002d1b25dc9da380d
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2021-12-28 15:16:45 +0500

    Up AGGREGATE FUNCTION with Practice

--- AGGREGATE FUNCTIONS.sql
+++ AGGREGATE FUNCTIONS.sql
@@ -39,3 +39,54 @@ From Sales.SalesOrderHeader
 
 SELECT COUNT(TotalDue)
 From Sales.SalesOrderHeader
+
+SELECT *
+FROM Sales.SalesOrderHeader
+WHERE OrderDate BETWEEN '1/1/2006' AND '12/31/2006'
+
+
+SELECT SUM(TotalDue)
+FROM Sales.SalesOrderHeader
+WHERE OrderDate BETWEEN '1/1/2006' AND '12/31/2006'
+
+
+
+SELECT MAX(FirstName)
+FROM Person.Person
+
+
+
+SELECT MIN(FirstName)
+FROM Person.Person
+
+SELECT COUNT(*)
+FROM Person.Person
+
+SELECT *
+FROM Person.Person
+
+SELECT COUNT(MiddleName)
+FROM Person.Person
+
+
+SELECT MiddleName
+FROM Person.Person
+WHERE MiddleName IS NOT NULL
+
+SELECT AVG(StandardCost)
+FROM Production.Product
+WHERE StandardCost > 0
+
+
+SELECT AVG(Freight)
+FROM Sales.SalesOrderHeader
+WHERE TerritoryID = 4
+
+SELECT MAX(ListPrice)
+FROM Production.Product
+
+SELECT SUM(P.ListPrice*I.Quantity)
+FROM Production.Product P
+INNER JOIN Production.ProductInventory I
+ON I.ProductID = P.ProductID
+WHERE P.ListPrice > 0
