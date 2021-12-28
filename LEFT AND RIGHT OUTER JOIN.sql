0a62dc4fd21e826aab70e3d7ebf460d32ce6171b
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2021-12-28 11:58:43 +0500

    Update LEFT AND RIGHT OUTER JOIN.sql

--- LEFT AND RIGHT OUTER JOIN.sql
+++ LEFT AND RIGHT OUTER JOIN.sql
@@ -35,3 +35,78 @@ LEFT OUTER JOIN Person.Person P
 ON P.BusinessEntityID = E.BusinessEntityID
 LEFT OUTER JOIN Sales.SalesTerritory T
 ON T.TerritoryID = SOH.TerritoryID
+
+--
+SELECT 
+P.FirstName,P.LastName,
+SOH.SalesOrderNumber,
+SOH.TotalDue AS SalesAmount,
+T.Name AS TerritoryName
+FROM Sales.SalesOrderHeader SOH
+LEFT OUTER JOIN Sales.SalesPerson SP
+ON SP.BusinessEntityID = SOH.SalesPersonID
+LEFT OUTER JOIN HumanResources.Employee E
+ON E.BusinessEntityID = SP.BusinessEntityID
+LEFT OUTER JOIN Person.Person P
+ON P.BusinessEntityID = E.BusinessEntityID
+LEFT OUTER JOIN Sales.SalesTerritory T
+ON T.TerritoryID = SOH.TerritoryID
+WHERE T.Name='Northwest'
+
+--
+SELECT 
+P.FirstName,P.LastName,
+SOH.SalesOrderNumber,
+SOH.TotalDue AS SalesAmount,
+T.Name AS TerritoryName
+FROM Sales.SalesOrderHeader SOH
+LEFT OUTER JOIN Sales.SalesPerson SP
+ON SP.BusinessEntityID = SOH.SalesPersonID
+LEFT OUTER JOIN HumanResources.Employee E
+ON E.BusinessEntityID = SP.BusinessEntityID
+LEFT OUTER JOIN Person.Person P
+ON P.BusinessEntityID = E.BusinessEntityID
+LEFT OUTER JOIN Sales.SalesTerritory T
+ON T.TerritoryID = SOH.TerritoryID
+WHERE T.Name='Northwest'
+ORDER BY 4 DESC
+
+--
+
+
+SELECT 
+P.FirstName,P.LastName,
+SOH.SalesOrderNumber,
+SOH.TotalDue AS SalesAmount,
+T.Name AS TerritoryName
+FROM Sales.SalesOrderHeader SOH
+LEFT OUTER JOIN Sales.SalesPerson SP
+ON SP.BusinessEntityID = SOH.SalesPersonID
+LEFT OUTER JOIN HumanResources.Employee E
+ON E.BusinessEntityID = SP.BusinessEntityID
+LEFT OUTER JOIN Person.Person P
+ON P.BusinessEntityID = E.BusinessEntityID
+LEFT OUTER JOIN Sales.SalesTerritory T
+ON T.TerritoryID = SOH.TerritoryID
+WHERE T.Name='Northwest'
+ORDER BY SalesAmount DESC
+
+
+--
+SELECT 
+P.FirstName,P.LastName,
+SOH.SalesOrderNumber,
+SOH.TotalDue AS SalesAmount,
+T.Name AS TerritoryName
+FROM Sales.SalesOrderHeader SOH
+LEFT OUTER JOIN Sales.SalesPerson SP
+ON SP.BusinessEntityID = SOH.SalesPersonID
+LEFT OUTER JOIN HumanResources.Employee E
+ON E.BusinessEntityID = SP.BusinessEntityID
+LEFT OUTER JOIN Person.Person P
+ON P.BusinessEntityID = E.BusinessEntityID
+LEFT OUTER JOIN Sales.SalesTerritory T
+ON T.TerritoryID = SOH.TerritoryID
+WHERE T.Name='Northwest'
+ORDER BY TotalDue DESC
+
