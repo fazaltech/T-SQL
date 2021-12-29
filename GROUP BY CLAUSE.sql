f868483d2eb47187cafa7890a72dcc9398ec8390
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2021-12-29 14:24:02 +0500

    Update GROUP BY CLAUSE.sql

--- GROUP BY CLAUSE.sql
+++ GROUP BY CLAUSE.sql
@@ -25,3 +25,21 @@ GROUP BY TerritoryID,SalesPersonID
 ORDER BY 1,2
 
 SELECT * FROM syslogins 
+
+
+
+SELECT 
+ST.Name AS[Territory Name],
+P.FirstName+' '+P.LastName AS SalesPersonName,
+
+SUM(TotalDue) AS [Total Sales]
+FROM Sales.SalesOrderHeader SOH
+INNER JOIN Sales.SalesPerson SP
+ON SP.BusinessEntityID=SOH.SalesPersonID
+INNER JOIN Person.Person P
+ON P.BusinessEntityID=SP.BusinessEntityID
+INNER JOIN Sales.SalesTerritory ST
+ON ST.TerritoryID=SOH.TerritoryID
+WHERE OrderDate BETWEEN '1/1/2006' AND '12/31/2006'
+GROUP BY ST.Name,P.FirstName+' '+P.LastName
+ORDER BY 1,2
