5fe3998a8921f841fe80e3dfb260ac73baf3d2ad
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2022-01-04 00:09:34 +0500

    Add SQL Server functions

---
+++ SQL Server functions.sql
@@ -0,0 +1,15 @@
+--Sub String
+SELECT 'Brewster', LEFT ('Brewster' ,4)
+
+
+SELECT TOP 100*
+FROM Person.Person
+
+SELECT LEFT(LastName,5)
+FROM Person.Person
+
+SELECT LEFT(LastName,5),LastName
+FROM Person.Person
+
+SELECT RIGHT(LastName,5) AS[Last Five Character of Last Name]
+FROM Person.Person
