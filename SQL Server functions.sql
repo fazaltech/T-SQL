211b1413fe06f9f98e316f56ec607fe8ef4aca98
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2022-01-04 12:53:32 +0500

    Update Sub String

--- SQL Server functions.sql
+++ SQL Server functions.sql
@@ -1,4 +1,4 @@
---Sub String
+
 SELECT 'Brewster', LEFT ('Brewster' ,4)
 
 
@@ -12,4 +12,10 @@ SELECT LEFT(LastName,5),LastName
 FROM Person.Person
 
 SELECT RIGHT(LastName,5) AS[Last Five Character of Last Name]
+FROM Person.Person
+
+--Sub String
+SELECT TOP 100 
+		FirstName,
+		SUBSTRING(FirstName,3,5)
 FROM Person.Person
