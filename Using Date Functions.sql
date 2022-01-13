1475b8aed3749b10eccbd43902375b784ae29981
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2022-01-13 14:50:28 +0500

    ADD ADDED DATE

--- Using Date Functions.sql
+++ Using Date Functions.sql
@@ -12,3 +12,7 @@ SELECT  DATEDIFF(MM,'7/1/1925','7/1/1927')
 
 --Return the difference between two time values, in hour
 SELECT  DATEDIFF(HOUR,'7/1/1925 16:14:00','7/1/1925 22:14:00')
+
+
+--Return the ADDED DATE
+SELECT DATEADD(DAY,30,'4/7/2011')
