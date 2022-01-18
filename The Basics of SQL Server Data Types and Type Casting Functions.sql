428ee5c1f92337c2332b17c5615bbd4ff9e7671d
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2022-01-18 16:14:04 +0500

    Add Data Types and Type Casting Functions
    Add The Basics of SQL Server Data Types and Type Casting Functions

---
+++ The Basics of SQL Server Data Types and Type Casting Functions.sql
@@ -0,0 +1,2 @@
+SELECT GETDATE(),CAST(GETDATE() AS DATE)AS Casted_DateSELECT GETDATE(),CONVERT (DATE,GETDATE())AS Type_Casted_DateSELECT TOP 100 FirstName FROM Person.PersonSELECT TOP 100 FirstName FROM Person.Person--Conversion failed when converting the nvarchar value 'Syed' to data type int.
+SELECT TOP 100 CAST(FirstName AS INT) FROM Person.Person
