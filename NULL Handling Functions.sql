5c34c25de8c833ca6328d8bff6015dae99ead677
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2022-01-18 14:34:29 +0500

    Update NULL Handling Functions.sql

--- NULL Handling Functions.sql
+++ NULL Handling Functions.sql
@@ -9,7 +9,16 @@ COALESCE(NULLIF(BillToAddressID,ShipToAddressID),1)
 FROM Sales.SalesOrderHeader
 
 
+
+--1) If the Title column of Person.Person is NULL then return the string “No Title Listed”.
+
 SELECT
 Title,COALESCE(Title, 'No Title Listed') AS Title
+FROM Person.Person
+
+--2) If the MiddleName column of Person.Person is NULL then return the string “No Middle Name Listed”.
+
+SELECT
+MiddleName,COALESCE(MiddleName, 'No Middle Name Listed') AS MiddleName
 FROM Person.Person
 
