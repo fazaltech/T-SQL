60e4b5913ead5fd37328b6901114743a93ff7e1a
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2022-01-18 12:55:40 +0500

    Update NULL Handling Functions.sql

--- NULL Handling Functions.sql
+++ NULL Handling Functions.sql
@@ -3,3 +3,7 @@
 SELECT TOP 100
 			FirstName, COALESCE(MiddleName,'')AS MiddleName,LastName
 FROM Person.Person
+
+SELECT BillToAddressID,ShipToAddressID,
+COALESCE(NULLIF(BillToAddressID,ShipToAddressID),1)
+FROM Sales.SalesOrderHeader
