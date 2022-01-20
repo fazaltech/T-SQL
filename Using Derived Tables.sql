3212ff4a26cfe872e141df431c5d3993ee727d9f
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2022-01-20 13:55:57 +0500

    Update Using Derived Tables.sql

--- Using Derived Tables.sql
+++ Using Derived Tables.sql
@@ -15,3 +15,43 @@ GROUP BY OrderYear
 SELECT *
 FROM (SELECT BusinessEntityID,NationalIDNumber,YEAR(BirthDate)AS BirthYear,YEAR(HireDate)AS HireYear FROM HumanResources.Employee) AS HR_Employee
 WHERE BirthYear < 1960
+
+
+SELECT *
+FROM (SELECT BusinessEntityID,NationalIDNumber,YEAR(BirthDate)AS BirthYear,YEAR(HireDate)AS HireYear FROM HumanResources.Employee) AS HR_Employee
+WHERE HireYear >= 2004
+
+
+SELECT *
+FROM(
+	SELECT BusinessEntityID,NationalIDNumber,BirthYear,YEAR(HireDate) HireYear 
+	FROM(SELECT BusinessEntityID,NationalIDNumber,YEAR(BirthDate) AS BirthYear,HireDate FROM HumanResources.Employee
+	)AS InterNested)AS OuterNested
+	
+
+
+SELECT *
+FROM(
+	SELECT BusinessEntityID,NationalIDNumber,BirthYear,YEAR(HireDate) HireYear 
+	FROM(SELECT BusinessEntityID,NationalIDNumber,YEAR(BirthDate) AS BirthYear,HireDate FROM HumanResources.Employee
+	)AS InterNested
+	WHERE BirthYear >1960
+	)AS OuterNested
+	WHERE HireYear >2003
+
+
+
+
+SELECT Sales_by_Year.SalesYear,Sales_by_Year.TotalRevenue,Hire_by_Year.NewHireCount
+FROM(SELECT SalesYear, SUM(TotalDue) AS TotalRevenue
+	FROM (SELECT YEAR(OrderDate) AS SalesYear,TotalDue FROM Sales.SalesOrderHeader)AS SalesDetails
+GROUP BY SalesYear
+)AS Sales_by_Year
+LEFT OUTER JOIN(SELECT HireYear,COUNT(BusinessEntityID)AS NewHireCount
+	FROM(SELECT YEAR(HireDate) AS HireYear,BusinessEntityID
+	FROM HumanResources.Employee)AS EmployeeDetails
+	GROUP BY HireYear
+)AS Hire_by_Year
+ON Hire_by_Year.HireYear = Sales_by_Year.SalesYear
+ORDER BY 1
+
