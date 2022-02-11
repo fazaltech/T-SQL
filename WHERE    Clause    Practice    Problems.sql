52267f475bdc52c771e762a8bdd247dc09ea9309
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2022-02-11 16:03:28 +0500

    Update WHERE    Clause    Practice    Problems.sql

--- WHERE    Clause    Practice    Problems.sql
+++ WHERE    Clause    Practice    Problems.sql
@@ -199,3 +199,16 @@ SELECT SalesPersonID,TotalDue
 FROM Sales.SalesOrderHeader
 WHERE SalesPersonID IS NOT NULL 
 AND TotalDue >70000
+
+WHERE CustomerName LIKE 'a%'	
+--Finds any values that start with "a"
+WHERE CustomerName LIKE '%a'	
+--Finds any values that end with "a"
+WHERE CustomerName LIKE '%or%'	
+--Finds any values that have "or" in any position
+WHERE CustomerName LIKE '_r%'	
+--Finds any values that have "r" in the second position
+WHERE CustomerName LIKE 'a__%'	
+--Finds any values that start with "a" and are at least 3 characters in length
+WHERE ContactName LIKE 'a%o'	
+--Finds any values that start with "a" and ends with "o"
